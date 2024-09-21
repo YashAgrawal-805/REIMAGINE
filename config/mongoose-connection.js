1f75c83a024d4cbc07a072868666e6c02f4f0911
YashAgrawal-805 <123628882+YashAgrawal-805@users.noreply.github.com>
2024-09-21 19:55:54 +0530

    Update mongoose-connection.js

--- config/mongoose-connection.js
+++ config/mongoose-connection.js
@@ -2,9 +2,8 @@ const mongoose = require("mongoose");
 const config = require("config");
 const dbgr = require("debug")("development:mongoose");
 
-const MONGODB_URI = config.get("MONGODB_URI");
 
-mongoose.connect(`${MONGODB_URI}`)
+mongoose.connect("mongodb+srv://yashagr805:tpTkaOUKLkQhBQIu@cluster0.h8e8k.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
     .then(() => dbgr("Successfully connected to MongoDB"))
     .catch((err) => dbgr(`Failed to connect to MongoDB: ${err.message}`));
 
